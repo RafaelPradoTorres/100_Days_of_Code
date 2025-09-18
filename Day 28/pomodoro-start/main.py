66c11f4deacef03b1a9cf64ecd21839a08b14c77
Rafael Prado <rafael.pradt8@gmail.com>
2025-09-18 14:05:12 -0300

    18/09/2025
    Projeto pomodoro
    Aprendizado biblioteca tkinter para GUI
    
    Argumentos opcionais em funções, *args e **kwargs

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -9,16 +9,44 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+
+    reps += 1
+
+    if reps % 8 == 0:
+        title.config(text="Long Break", fg=GREEN)
+        count_down(long_break_sec)
+    elif reps % 2 == 1:
+        title.config(text="Working", fg=RED)
+        count_down(work_sec)
+    else:
+        title.config(text="Short Break", fg=PINK)
+        count_down(short_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    global reps
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -27,8 +55,16 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count>0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,11 +87,11 @@ title.grid(column=1, row=0)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mark = Label(text="✔", fg=GREEN, bg=YELLOW)
-check_mark.grid(column=1, row=3)
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 
 
