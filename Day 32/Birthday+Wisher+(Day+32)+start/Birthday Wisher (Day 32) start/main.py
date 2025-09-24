360bab8d886a53d3e28507f8b610145725a3df9d
Rafael Prado <rafael.pradt8@gmail.com>
2025-09-24 14:32:26 -0300

    Script para enviar emails de aniversário automaticamente

--- Day 32/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ Day 32/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,4 +1,5 @@
-import smtplib
+import smtplib, datetime as dt
+from random import choice
 
 my_email = "testerafa04@gmail.com"
 password = "bkhm ycpm uimz qnwz"
@@ -8,9 +9,23 @@ password = "bkhm ycpm uimz qnwz"
 # connection.login(user=my_email, password=password)
 # connection.sendmail(from_addr=my_email, to_addrs="rafateste04@outlook.com", msg="Subject:OMG!! Hiii!\n\nHi, fella!!")
 # connection.close()
+day_of_week = dt.datetime.now().weekday()
+if day_of_week == 1:
+    with open('./quotes.txt', 'r') as quote_file:
+        quotes = quote_file.readlines()
+        message = choice(quotes)
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls() #secure the connection
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="rafateste04@outlook.com", msg="Subject:OMG!! Hiii!\n\nHi, fella!!")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls() #secure the connection
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="rafateste04@outlook.com", msg=f"Subject:Vamos, Rafa!\n\n{message}")
 
+# import datetime as dt
+#
+# now = dt.datetime.now() #datetime object
+# year = now.year #int
+# month = now.month
+# day_of_week = now.weekday()
+#
+# date_of_birth = dt.datetime(year=2004, month=12, day=29, hour=18)
+# print(date_of_birth)
---
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,45 @@
+##################### Extra Hard Starting Project ######################
+import pandas as pd
+import datetime as dt
+from random import randint
+import smtplib
+
+# 1. Update the birthdays.csv
+data = pd.read_csv("./birthdays.csv")
+today = dt.datetime.today()
+
+# 2. Check if today matches a birthday in the birthdays.csv
+today = (today.month, today.day)
+
+
+birthday_dict = {(data_row.month, data_row.day):data_row for (index, data_row) in data.iterrows()}
+try:
+    niver_de_hoje = birthday_dict[today]
+
+except KeyError:
+    print("Sem aniversários hoje, amigo!!!")
+else:
+    # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME]
+    # with the person's actual name from birthdays.csv
+    file = f"./letter_templates/letter_{randint(1,3)}.txt"
+    with open(file) as letter:
+        content = letter.read()
+
+        content = content.replace("[NAME]", niver_de_hoje["name"])
+
+
+    my_email = "testerafa04@gmail.com"
+    password = "bkhm ycpm uimz qnwz"
+
+
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr = my_email,
+                            to_addrs = niver_de_hoje["email"],
+                            msg = f"Subject: Happy birthday!\n\n{content}")
+
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
